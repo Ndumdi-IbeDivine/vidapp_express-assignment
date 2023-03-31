11d3e4f595a84d73c6e84a59501cc438a68f47df
Ndumdi-IbeDivine <dvineonyi@gmail.com>
2023-03-31 21:22:45 +0100

    customer/user authentication

--- src/models/customers.js
+++ src/models/customers.js
@@ -1,34 +1,34 @@
-const joi = require('joi');
+const Joi = require('joi');
 const mongoose = require('mongoose');
 
-const customerSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        minlength: 5,
-        maxlength: 50
-    },
-    isGold: {
-        type: Boolean,
-        default: false,
-    },
-    phone: {
-        type: String,
-        required: true,
-        minlength: 5,
-        maxlength: 50
-    }
-})
-
-const validateCustomer = (customer) => {
-    const schema = {
-      name: Joi.string().min(5).max(50).required(),
-      phone: Joi.string().min(5).max(50).required(),
-      isGold: Joi.boolean()
-    };
-  
-    return Joi.validate(customer, schema);
+const Customer = mongoose.model('Customer', new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 50
+  },
+  isGold: {
+    type: Boolean,
+    default: false
+  },
+  phone: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 50
   }
+}));
 
-module.exports = mongoose.model("Customers", customerSchema);
-module.exports = mongoose.model(validateCustomer);
+function validateCustomer(customer) {
+  const schema = {
+    name: Joi.string().min(5).max(50).required(),
+    phone: Joi.string().min(5).max(50).required(),
+    isGold: Joi.boolean()
+  };
+
+  return Joi.validate(customer, schema);
+}
+
+module.exports.Customer = Customer; 
+module.exports.validate = validateCustomer;
